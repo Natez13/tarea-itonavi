3c91c8287b86d5d55ee869ab8f715b3e33485155
Natez <70248621+Natez13@users.noreply.github.com>
2024-06-03 08:03:35 +0000

    test app

--- api/index.py
+++ api/index.py
@@ -2,17 +2,17 @@
 
 from flask import Flask, render_template
 from flask import send_from_directory
-#
-#from routes.products_routes import product_bp
-#from flask_cors import CORS
+
+from routes.products_routes import product_bp
+from flask_cors import CORS
 
 app = Flask(__name__)
-#CORS(app, origins='https://friendly-space-funicular-xxq5x7jxggg296j5-5000.app.github.dev')
+CORS(app, origins='https://friendly-space-funicular-xxq5x7jxggg296j5-5000.app.github.dev')
  
 app.static_folder = 'static'
 
 # Registrar Blueprint
-#app.register_blueprint(product_bp)
+app.register_blueprint(product_bp)
 
 @app.route('/styles.css')
 def styles():
@@ -20,8 +20,8 @@ def styles():
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-    #return render_template('index.html')
+    #return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
