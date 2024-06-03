e66d318e40606a41a8c85d5551e9a57eda15742c
Natez <70248621+Natez13@users.noreply.github.com>
2024-06-03 00:58:48 +0000

    test

--- api/index.py
+++ api/index.py
@@ -20,7 +20,8 @@ def styles():
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'Hello, World!'
+    #return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
