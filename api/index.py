ea33ef6525fa92c30ac26cbda1a42f363ef8718b
Natez <70248621+Natez13@users.noreply.github.com>
2024-06-03 19:10:31 +0000

    test index

--- api/index.py
+++ api/index.py
@@ -7,19 +7,19 @@ from api.products_routes import product_bp
 
 app = Flask(__name__)
  
-#app.static_folder = 'static'
+app.static_folder = 'static'
 
 # Registrar Blueprint
 app.register_blueprint(product_bp, url_prefix='/api')
 
-#@app.route('/styles.css')
-#def styles():
-    #return send_from_directory('static', 'styles.css', mimetype='text/css')
+@app.route('/styles.css')
+def styles():
+    return send_from_directory('static', 'styles.css', mimetype='text/css')
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-    #return render_template('index.html')
+    #return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
